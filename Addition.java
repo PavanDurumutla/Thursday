1ba8e6e515f63045e9036e02c2ccc468e87bb239
PavanDurumutla <121231639+PavanDurumutla@users.noreply.github.com>
2022-12-22 17:05:43 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package codes;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int sum=a+b;
+		
+		System.out.println(sum);
+		System.out.println(sum*sum);
+	}
+
+}
